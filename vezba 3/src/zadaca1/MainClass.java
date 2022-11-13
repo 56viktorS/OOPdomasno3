90e5290a57fbb95534dd1954c29fe5308fe04806
unknown <viktor.sterjovski@yahoo.com>
2022-11-13 21:52:44 +0100

    Postavuvanje 3to domasno

---
+++ vezba 3/src/zadaca1/MainClass.java
@@ -0,0 +1,13 @@
+package zadaca1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Avtomobil av=new Avtomobil("Mazda","Opel","Lancia",218380, 1998, 
+				"BT-0000-AA");
+				av.pMetod();
+				System.out.println(av.vMetod());
+				
+	}
+
+}
---
+++ vezba 3/src/zadaca4/Brojac.java
@@ -0,0 +1,18 @@
+package zadaca4;
+
+public class Brojac {
+
+	public static void main(String[] args) {}
+		
+		public int brojach;
+		
+		public void zgolemi () {
+			this.brojach++;
+		}
+		
+		public void reset () {
+			this.brojach = 0;
+	
+	}
+
+}
